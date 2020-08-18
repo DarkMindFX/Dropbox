6cc7bc7ca0d0503a4c92ca315e3aa0a45c4e8998
DarkMindFx <contact@darkmindfx.com>
2020-08-18 09:33:36 +0200

    Revert "Revert "Docker image for globus-users-api service""
    This reverts commit 224bbe1f1f031bb115dd5f5559d6981c11dbe6cb.

--- globus-users-api/src/main/java/org/globus/users/api/UsersApiEntryPoint.java
+++ globus-users-api/src/main/java/org/globus/users/api/UsersApiEntryPoint.java
@@ -1,12 +1,25 @@
 package org.globus.users.api;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
-public class UsersApiEntryPoint {
+public class UsersApiEntryPoint extends SpringBootServletInitializer {
 
     public static void main(String args[]) {
         SpringApplication.run(UsersApiEntryPoint.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UsersApiEntryPoint.class);
+    }
+
 }
